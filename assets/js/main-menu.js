cca32a84c1b2c8db44eb1ebd9c376ace4fd4ea07
NSmocker <sinchik97@gmail.com>
2024-05-05 18:07:50 +0300

    Last changes

--- assets/js/main-menu.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('toggleBtn').addEventListener('click', function() {
-  var sidebar = document.getElementById('sidebar');
-  if (sidebar.style.left === '-200px') {
-    sidebar.style.left = '0';
-  } else {
-    sidebar.style.left = '-200px';
-  }
-});
