f8bcbc3ee3562ac14764e4852a6fdcf2ff6575e2
NSmocker <sinchik97@gmail.com>
2024-05-06 03:13:27 +0300

    Last Changes

---
+++ assets/js/models-viewport.js
@@ -0,0 +1,12 @@
+function   hideIframe() {
+    document.getElementById("iframe-containter").style.display = "none";
+}
+
+function showTeleporter() 
+{   document.getElementById("iframe-containter").style.display = "flex";
+    document.getElementById("model_viewport").setAttribute("src", "https://sketchfab.com/models/d05b1b63ced04945852e35418f5c2407/embed");
+}   
+function showTree() 
+{   document.getElementById("iframe-containter").style.display = "flex";
+    document.getElementById("model_viewport").setAttribute("src", "https://sketchfab.com/models/642c8138a00f4c50a1516a308734dd65/embed");
+}   
