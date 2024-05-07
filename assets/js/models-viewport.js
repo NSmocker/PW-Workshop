acc0471c4476d69d0112647bcd564e14f9c6224c
NSmocker <sinchik97@gmail.com>
2024-05-07 12:31:46 +0300

    123

--- assets/js/models-viewport.js
+++ assets/js/models-viewport.js
@@ -10,3 +10,13 @@ function showTree()
 {   document.getElementById("iframe-containter").style.display = "flex";
     document.getElementById("model_viewport").setAttribute("src", "https://sketchfab.com/models/97b73d521eb3464e99dc4aa010437240/embed");
 }   
+function showRedLotus()
+{
+    document.getElementById("iframe-containter").style.display = "flex";
+    document.getElementById("model_viewport").setAttribute("src", "https://sketchfab.com/models/7974c5be98054c6098907ca169b910d0/embed");
+}
+function showJeenTaiBo()
+{
+    document.getElementById("iframe-containter").style.display = "flex";
+    document.getElementById("model_viewport").setAttribute("src", "https://sketchfab.com/models/31368a25329548b8aaafc486ec6f4d04/embed");
+}
